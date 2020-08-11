a6c301ad44ba3f0e162c714bcc2cd67f9ea819b0
Kyle Nakamura <kjnakamura@apu.edu>
2020-08-11 15:27:14 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <!-- Import basic styling from Sakura CSS -->
+        <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css" type="text/css">
+    </head>
+
+    <body>
+        <h1>Welcome to Apache</h1>
+        <p>This file is located at /var/www/html/index.php</p>
+
+        <?php
+        phpinfo();
+        ?>
+    </body>
+</html>

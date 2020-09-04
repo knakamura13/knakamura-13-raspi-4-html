329e31d1504463284b8a751906bdeef9e074b4c3
Kyle Nakamura <kjnakamura@apu.edu>
2020-09-04 14:06:15 -0700

    Add jquery, sakura to index.php

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
-<html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
     <head>
-        <!-- Import basic styling from Sakura CSS -->
+        <title>Raspberry Pi - index.php</title>
+
+        <!-- Import jQuery 3.5.1 -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+
+        <!-- Import Sakura CSS -->
         <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css" type="text/css">
     </head>
 
     <body>
-        <h1>Welcome to Apache</h1>
-        <p>This file is located at /var/www/html/index.php</p>
-
-        <?php
-        phpinfo();
-        ?>
+        <div id="main">
+            <h2>Welcome to the Pi server!</h2>
+        </div>
     </body>
 </html>
+
+<script type="text/javascript">
+    $(document).ready(() => {
+        console.log(`Welcome to the Pi server!`);
+    });
+</script>
+
+<style type="text/css">
+#main {
+}
+</style>
